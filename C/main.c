8a7d96424574d7bac8b0b46895fa97175e91c5c4
lbm1202 <junghee@zete.co.kr>
2022-09-20 22:55:26 +0900

    edit code

--- C/main.c
+++ C/main.c
@@ -1,11 +1,17 @@
 #include <stdio.h>
 
+void fun(void){
+    printf("fun called\n");
+}
+
 int main(void){
     printf("Hello World!\n");
 
     printf("Test commit\n");
 
-    printf("Edit iPad air 4\n")
+    printf("Edit iPad air 4\n");
+    
+    fun();
 
     return 0;
 }
